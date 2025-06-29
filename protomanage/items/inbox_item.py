d27eafb130b8701ce5c1efc7494398d896fb435d
Thomas O'Dea <thomodea@amd.com>
2025-06-28 23:06:27 -0400

    no longer using a dataclass due to bugs with metaclass

--- protomanage/items/inbox_item.py
+++ protomanage/items/inbox_item.py
@@ -2,18 +2,19 @@
 
 from dataclasses import dataclass
 
-from .item import Item
+from .item import Item,ItemMeta
 from ..execution_context import ExecutionContext
 
-@dataclass
-class InboxItem(Item):
+class InboxItem(Item,metaclass=ItemMeta):
     """The simplest item: an inbox item."""
-    context: ExecutionContext = None
-    text: str = ""
 
     DISPLAY_NAME = "Inbox Item"
     UNIQUE_NAME = "protomanage.core.inbox-item"
     VERSION = "0.1"
+
+    def __init__(self,context : ExecutionContext, text : str):
+        self.context = context
+        self.text = text
 
     def __str__(self) -> str:
         """Return a string representation of the InboxItem."""
