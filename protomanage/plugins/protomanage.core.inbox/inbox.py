447fce6bbed24a0e1c6d4012f85486d4e9e4538d
Thomas O'Dea <thomodea@amd.com>
2025-07-04 13:06:07 -0400

    inbox plugin

--- protomanage/plugins/protomanage.core.inbox/inbox.py
+++ protomanage/plugins/protomanage.core.inbox/inbox.py
@@ -14,13 +14,9 @@ a source of stress rather than a place of calm.
 """
 
 from typing import List
-import typer
 
-from ..item import Item,ItemMeta
-from ..execution_context import ExecutionContext
-from .. import cli
-from .. import repo as repo_lib
-from .. import strings # TOOD migrate this to local strings
+from protomanage.base.item import Item,ItemMeta
+from protomanage.base.execution_context import ExecutionContext
 
 class InboxItem(Item,metaclass=ItemMeta):
     """The simplest item: an inbox item."""
@@ -29,7 +25,7 @@ class InboxItem(Item,metaclass=ItemMeta):
     UNIQUE_NAME = "protomanage.core.inbox-item"
     VERSION = "0.1"
 
-    def __init__(self,context : ExecutionContext, text : str):
+    def __init__(self,context : "ExecutionContext", text : str):
         self.context = context
         self.text = text
 
@@ -51,27 +47,21 @@ class InboxItem(Item,metaclass=ItemMeta):
 
         context = ExecutionContext.from_dict(data["context"])
         text = data["text"]
-        return cls(context=context, text=text)
 
-def _add_to_inbox(text : str, repo : repo_lib.Repo, context : ExecutionContext) -> None:
+        instance = cls.__new__(cls)
+        instance.context = context
+        instance.text = text
+
+        return instance
+
+def _add_to_inbox(text : str, repo : "Repo", context : "ExecutionContext") -> None:
     """Add a simple InboxItem to the repo's item list."""
 
     item = InboxItem(context=context, text=text)  # Create an InboxItem instance
-    cli.current_repo.add_item(item)  # Add the item to the repo's items
+    repo.add_item(item)  # Add the item to the repo's items
     #cli.logger.info(strings.INBOX_ITEM_ADDED) TODO add logging here
 
-@cli.app.command()
-def inbox(text: List[str]) -> None:
-    """Add a simple InboxItem to the repo's item list."""
-
-    _add_to_inbox(
-        text = " ".join(text),
-        repo = cli.current_repo,
-        context = cli.execution_context
-    )
-    #current_repo._logger.info(strings.INBOX_ITEM_ADDED) TODO add logging here
-
-def _show_inbox(repo : repo_lib.Repo, context : ExecutionContext) -> None:
+def _show_inbox(repo : "Repo", context : "ExecutionContext") -> None:
     """Retrieve all InboxItems from the repo's item list and display to the user"""
 
     inbox_items = [item for item in repo.items if isinstance(item,InboxItem)]
@@ -79,13 +69,30 @@ def _show_inbox(repo : repo_lib.Repo, context : ExecutionContext) -> None:
         print("No items in inbox!")
     else:
         for item in inbox_items:
-            print(str(item))
+            print(str(item)) # TODO don't use print, return it according to the application context
 
-@cli.app.command()
-def show_inbox() -> None:
-    """Retrieve all InboxItems from the repo's items list."""
+def _configure_cli_app(app,current_repo,execution_context):
+    @app.command(rich_help_panel="Inbox")
+    def show_inbox() -> None:
+        """Retrieve all InboxItems from the repo's items list."""
 
-    _show_inbox(
-        repo = cli.current_repo,
-        context = cli.execution_context
-    )
+        _show_inbox(
+            repo = current_repo,
+            context = execution_context
+        )
+
+    @app.command(rich_help_panel="Inbox")
+    def inbox(text: List[str]) -> None:
+        """Add a simple InboxItem to the repo's item list."""
+
+        _add_to_inbox(
+            text = " ".join(text),
+            repo = current_repo,
+            context = execution_context
+        )
+        #current_repo._logger.info(strings.INBOX_ITEM_ADDED) TODO add logging here
+
+def configure_app(app,current_repo,execution_context):
+    """TODO docs write docstring"""
+    # TODO detect (or pass in?) the app type
+    _configure_cli_app(app,current_repo,execution_context)
