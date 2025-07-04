aed5a2456f4ee1271364d1ccf9c72cabb9e6042b
Thomas O'Dea <thomodea@amd.com>
2025-07-04 13:03:34 -0400

    setup.py to accommodate editable install

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="protomanage",
+    version="0.dev0",
+    python_requires=">=3.12",
+    package_dir={"": "."},
+    install_requires=[
+        # Add dependencies here if needed
+    ],
+)
